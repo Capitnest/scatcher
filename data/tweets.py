32cd220ed71f67ee1f69d0ae2f8e2a055bbd511b
timnik <72356939+nottimnik@users.noreply.github.com>
2022-06-02 18:57:08 +0000

    Update tweets.py

--- data/tweets.py
+++ data/tweets.py
@@ -1,2 +1,4 @@
+# some test data you can use
+
 usernames = ["BTC_Archive", "WatcherGuru"]
 profile_images = ["https://pbs.twimg.com/profile_images/1523608991422722049/NnAUKAZ-_400x400.jpg", "https://pbs.twimg.com/profile_images/1475973046133481477/AorQUcvD_400x400.jpg"]
